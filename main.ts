fbc8e3fccee822f25c9963918f96a149d16e7cb2
Scout Mosley <scout.mosley@stu.ocsb.ca>
2025-10-24 12:31:23 -0400

    adss one to score

--- main.ts
+++ main.ts
@@ -1,16 +1,18 @@
 /* Copyright (c) 2025 MTHS All rights reserved
  *
  * Created by: scout mosley
- * Created on: Sep 2025
- * This program adds one to counter when a is pressed
+ * Created on: Oct 2025
+ * This program adds one to counter when a is pressed.
 */
+
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-let numberOfCookies:number
+let cookiesPressed = 0
 
-
-input.buttonIsPressed(Button.A)
-basic.showNumber
-numberOfCookies=(numberOfCookies+1)
-
+// add 1
+input.onButtonPressed(Button.A, function () {
+    cookiesPressed += 1
+    basic.clearScreen()
+    basic.showNumber(cookiesPressed)
+})
